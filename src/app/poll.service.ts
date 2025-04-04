d2b5c40e8d5d1c115c38cf315e3275ccc0793405
Kavindu Dilshan <kavindu4543@gmail.com>
2025-04-04 23:57:26 +0530

    Creat Poll Frontend Completed

--- src/app/poll.service.ts
+++ src/app/poll.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Poll } from './poll.models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PollService {
 
-  constructor() { }
+  private baseUrl: string="http://localhost:8080/api/polls";
+
+  constructor(private http: HttpClient) { }
+
+  createPoll(poll:Poll): Observable<Poll>{
+    return this.http.post<Poll>(`${this.baseUrl}/add-poll`,poll);
+  }
+
+
+  getPolls(): Observable<Poll[]>{
+    return this.http.get<Poll[]>(`${this.baseUrl}/get-all-polls`);
+  }
+
+  vote(pollId: number,optionIndex: number):Observable<void>{
+    const url = `${this.baseUrl}/vote`;
+    return this.http.post<void>(url, { pollId, optionIndex });
+  }
+
 }
